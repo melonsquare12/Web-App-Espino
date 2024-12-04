e273d5c92ddebdb0c47d615187a5e010eecacd77
melonsquare12 <161678508+melonsquare12@users.noreply.github.com>
2024-12-04 15:33:46 +0800

    Update index.php

--- TodoApp/todo_app/index.php
+++ TodoApp/todo_app/index.php
@@ -2,7 +2,7 @@
 session_start();
 require 'db.php';
 
-// Redirect to dashboard if user is already logged in
+
 if (isset($_SESSION['user_id'])) {
     header("Location: dashboard.php");
     exit();
